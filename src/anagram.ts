e54a9059b439e6b7c038fcae045203af2a45f44d
Rudolph Jose Razul <rudolphjose.razul@timesoftsg.com.sg>
2024-05-21 14:28:25 +0800

    updated notes

--- src/anagram.ts
+++ src/anagram.ts
@@ -1,3 +1,12 @@
+/*
+An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. 
+eg 
+'silent' = 'listen', 
+'cheater' = 'teacher'
+
+Coding challenge: Write a function that checks if two strings are anagrams of each other.
+*/
+
 export function areAnagrams(str1: string, str2: string): boolean {
     //your code here
     return false;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { areAnagrams } from "./anagram";
 
-console.log("An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once. eg 'silent' = 'listen', 'cheater' = 'teacher' ");
-console.log("\nCoding challenge: Write a function that checks if two strings are anagrams of each other.");
 console.log("\nAnagram Checker:");
-
 console.log(areAnagrams("listen", "silent")); // true
 console.log(areAnagrams("engineer", "developer")); // false
 console.log(areAnagrams("cheater","teacher")); // true
